b19200482f3d63bf80b286db020be95cc1a2a18b
Ryan Goh <gohengkeat89@gmail.com>
2015-07-27 16:20:37 +0800

    set dev and prod email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'http://rails-jobby.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: ENV["JOBBY_EMAIL_USER"],
+        password: ENV["JOBBY_EMAIL_PASS"]
+
+    }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,13 +6,13 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '406e07621aa9ad55b52c527674e03b31d3eb1887fefa22d60590333adaa21c95459e19aa66630fd8a01f9ea1fb744a90cacee33dffe73451f8ebf7f823477f48'
+  config.secret_key = '406e07621aa9ad55b52c527674e03b31d3eb1887fefa22d60590333adaa21c95459e19aa66630fd8a01f9ea1fb744a90cacee33dffe73451f8ebf7f823477f48'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'codingshophouse@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
