2dffc7e90450a96a3c321a026ae30770d025e3e7
Ryan Goh <gohengkeat89@gmail.com>
2015-08-02 17:56:43 +0800

    generate avatar to user and edit model user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728140053) do
+ActiveRecord::Schema.define(version: 20150802094419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20150728140053) do
     t.string   "name"
     t.text     "job_requested"
     t.text     "job_posted"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
