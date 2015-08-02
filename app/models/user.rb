3cc0c489b27f83da9694f2f0aec60b7c639ec713
Ryan Goh <gohengkeat89@gmail.com>
2015-08-02 20:20:21 +0800

    add default profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/profilepic.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
