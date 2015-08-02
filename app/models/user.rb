bcad09399e768c98730b9cfd3fadda1e7a22fd2e
Ryan Goh <gohengkeat89@gmail.com>
2015-08-02 18:55:07 +0800

    add avatar validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
+  validates_attachment :avatar,   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
