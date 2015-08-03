35d8035e7be4c7cbb658cff00a9cdd5a629039a0
fzy1995 <fzy1995@gmail.com>
2015-08-03 20:11:42 +0800

    fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150728122551) do
-=======
-ActiveRecord::Schema.define(version: 20150728061128) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "duration"
@@ -33,7 +28,6 @@ ActiveRecord::Schema.define(version: 20150728061128) do
     t.datetime "updated_at",            null: false
   end
 
-=======
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -55,5 +49,4 @@ ActiveRecord::Schema.define(version: 20150728061128) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> master
 end
