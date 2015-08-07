f410c18424423875806952412bd682c530e38b47
Ryan Goh <gohengkeat89@gmail.com>
2015-08-07 12:27:19 +0800

    Add confirmable module to user, edit new migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/profilepic.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
