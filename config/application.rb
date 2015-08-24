57b59c64187b3329e8a5cf77713b3d45d38b34c8
Ryan Goh <gohengkeat89@gmail.com>
2015-08-24 16:08:23 +0800

    remove dotenv

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
-HOSTNAME = ENV['HOSTNAME']
 
 module Jobby
   class Application < Rails::Application
