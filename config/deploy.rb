74a34e78b4ddc216a487f765921e3a09760f5a39
Ryan Goh <gohengkeat89@gmail.com>
2015-08-24 15:55:58 +0800

    edit mina deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
@@ -10,29 +10,36 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :domain, 'vertdeal.com'
+set :deploy_to, '/home/ryan/project/Jobby'
+set :repository, 'https://github.com/ryangoh/Jobby.git'
 set :branch, 'master'
+set :term_mode, nil
+set :rails_env, 'production'
+set :branch, 'master'
+set :db_name, 'jobby_production2'
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+set :shared_paths, ['config/database.yml', 'log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
+set :user, 'ryan'
+set :port, '22'
+set :forward_agent, true
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+  invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
@@ -49,16 +56,20 @@ task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
 
-  queue %[
-    repo_host=`echo $repo | sed -e 's/.*@//g' -e 's/:.*//g'` &&
-    repo_port=`echo $repo | grep -o ':[0-9]*' | sed -e 's/://g'` &&
-    if [ -z "${repo_port}" ]; then repo_port=22; fi &&
-    ssh-keyscan -p $repo_port -H $repo_host >> ~/.ssh/known_hosts
-  ]
+  queue! %[touch "#{deploy_to}/.rbenv-vars"]
+  queue  %[echo "-----> Be sure to add your ENV VAR in '#{deploy_to}/.rbenv-vars'."]
+
+  invoke :'setup:db:database_yml'
+  # invoke :'create:database'
+  # queue %[
+  #   repo_host=`echo $repo | sed -e 's/.*@//g' -e 's/:.*//g'` &&
+  #   repo_port=`echo $repo | grep -o ':[0-9]*' | sed -e 's/://g'` &&
+  #   if [ -z "${repo_port}" ]; then repo_port=22; fi &&
+  #   ssh-keyscan -p $repo_port -H $repo_host >> ~/.ssh/known_hosts
+  # ]
 end
+
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
@@ -71,9 +82,12 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
+    invoke :'rails:db_create'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
+
+    invoke :restartng
 
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
@@ -82,9 +96,58 @@ task :deploy => :environment do
   end
 end
 
+
+desc "Populate shared database.yml"
+task :'setup:db:database_yml' => :environment do
+  database_yml = <<-DATABASE.dedent
+    #{rails_env}:
+      adapter: postgresql
+      encoding: unicode
+      database: #{db_name}
+      username: <%= ENV['DATABASE_USER'] %>
+      password: <%= ENV['DATABASE_PASS'] %>
+      host: localhost
+      timeout: 5000
+      pool: 5
+  DATABASE
+  queue! %{
+    echo "-----> Populating shared database.yml"
+    echo "#{database_yml}" > #{deploy_to!}/shared/config/database.yml
+    echo "-----> Done"
+  }
+end
+
+#restart nginx server
+task :restartng do
+  queue "sudo nginx -s reload"
+end
+
+task :logs do
+  queue 'sudo tail -f /var/log/nginx/error.log'
+end
+
+task :reboot do
+  queue "sudo reboot"
+end
+
+task :prodlog do
+  queue "tail -f #{deploy_to}/shared/log/production.log"
+end
 # For help in making your deploy script, see the Mina documentation:
 #
 #  - http://nadarei.co/mina
 #  - http://nadarei.co/mina/tasks
 #  - http://nadarei.co/mina/settings
 #  - http://nadarei.co/mina/helpers
+class String
+  def dedent
+    lines = split "\n"
+    return self if lines.empty?
+    indents = lines.map do |line|
+      line =~ /\S/ ? (line.start_with?(" ") ? line.match(/^ +/).offset(0)[1] : 0) : nil
+    end
+    min_indent = indents.compact.min
+    return self if min_indent.zero?
+    lines.map { |line| line =~ /\S/ ? line.gsub(/^ {#{min_indent}}/, "") : line }.join "\n"
+  end
+end
